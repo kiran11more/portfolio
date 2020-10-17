d48e8d9a8b9b77f94cf16a40507616662d62fca4
Kiran <kiran11more@gmail.com>
2020-10-18 02:54:38 +0530

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
-const navToggle = document.querySelector('.nav-toggle')
-const navLinks = document.querySelectorAll('.nav-link')
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
 
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open')
-})
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
 
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open')
-    })
-})
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 250) {
+		header.style.backgroundColor = '#29323c';
+	} else {
+		header.style.backgroundColor = 'transparent';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
 
 
 // back to top button
@@ -33,3 +46,17 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+//fancybox caption
+$('[data-fancybox="gallery"]').fancybox({
+	caption : function( instance, item ) {
+	  var caption = $(this).data('caption') || '';
+  
+	  if ( item.type === 'image' ) {
+		caption = '<a href="http://' + caption + '" target="_blank">' + caption + '</a>' ;
+	  }
+  
+	  return caption;
+	}
+  });
